ffff48602d31174013b8de7e68a08992931104d5
Freeezzzi <60979041+Freeezzzi@users.noreply.github.com>
2021-03-13 12:09:49 +0300

    add floating button

--- app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseListFragment.kt
+++ app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseListFragment.kt
@@ -1,5 +1,6 @@
 package com.github.android_academy.hackathon.ui.courselist
 
+import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
@@ -9,8 +10,10 @@ import com.github.android_academy.hackathon.App
 import com.github.android_academy.hackathon.R
 import com.github.android_academy.hackathon.databinding.CourseListFragmentBinding
 import com.github.android_academy.hackathon.di.viewmodels.courselist.DaggerCourseListViewModelComponent
+import com.github.android_academy.hackathon.domain.models.User
 import com.github.android_academy.hackathon.ui.BaseFragment
 import com.github.android_academy.hackathon.ui.loginfragment.LoginFragment
+import com.github.android_academy.hackathon.ui.registrationfragment.RegistrationFragment
 
 class CourseListFragment : BaseFragment(R.layout.course_list_fragment){
     private val binding by viewBinding(CourseListFragmentBinding::bind)
@@ -23,10 +26,21 @@ class CourseListFragment : BaseFragment(R.layout.course_list_fragment){
     override fun initViews(view: View) {
         super.initViews(view)
         //TODO observe
+
+        //recycler
+        val coursesAdapter = CoursesAdapter {
+            viewModel.onCourseAction(it)
+        }
+        binding.courseListFragmentRecycler.adapter = coursesAdapter
+
+        //fab
+        binding.courseFragmentFab.setOnClickListener {
+            viewModel.addCourseAction() }
     }
 
 
     companion object {
+        private const val KEY_USER = "user"
         @JvmStatic
         fun newInstance() = LoginFragment()
     }
--- app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseListViewModel.kt
+++ app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseListViewModel.kt
@@ -1,12 +1,21 @@
 package com.github.android_academy.hackathon.ui.courselist
 
 import androidx.lifecycle.ViewModel
+import com.github.android_academy.hackathon.domain.models.Course
 import com.github.terrakok.cicerone.Router
 import javax.inject.Inject
 
 class CourseListViewModel @Inject constructor(
-    private val router : Router
+    private val router : Router,
+
 ) : ViewModel(){
 
+    fun onCourseAction(course: Course){
+        //TODO обработка нажатия на курс
+    }
+
+    fun addCourseAction(){
+
+    }
     //TODO: implement view model
 }
