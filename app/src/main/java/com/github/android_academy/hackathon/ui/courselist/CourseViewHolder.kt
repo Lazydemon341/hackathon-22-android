be2116ce9a72141d1a87a7925ab4ad4246b86970
Alexander <vlas.s.341@gmail.com>
2021-03-13 20:10:57 +0300

    Update course list

--- app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseViewHolder.kt
+++ app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseViewHolder.kt
@@ -1,42 +1,51 @@
 package com.github.android_academy.hackathon.ui.courselist
 
+import android.content.res.ColorStateList
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.github.android_academy.hackathon.R
 import com.github.android_academy.hackathon.databinding.CourseItemFragmentBinding
 import com.github.android_academy.hackathon.domain.models.Course
 
-class CourseViewHolder(private val binding : CourseItemFragmentBinding):
-        RecyclerView.ViewHolder(binding.root) {
+class CourseViewHolder(private val binding: CourseItemFragmentBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     fun bind(
-        course : Course,
-        courseListener:(Course)-> Unit,
-        addToFavoriteListener : (Course) -> Unit
+        course: Course,
+        courseListener: (Course) -> Unit,
+        addToFavoriteListener: (Course) -> Unit
     ) {
-        setClickListeners(courseListener, addToFavoriteListener,course)
+        setClickListeners(courseListener, addToFavoriteListener, course)
         setText(course)
+
+        if (course.isSubscribed) {
+            ImageViewCompat.setImageTintList(
+                binding.courseFavouriteStar,
+                ColorStateList.valueOf(ContextCompat.getColor(itemView.context, R.color.gold))
+            )
+        }
+        else{
+            ImageViewCompat.setImageTintList(
+                binding.courseFavouriteStar,
+                ColorStateList.valueOf(ContextCompat.getColor(itemView.context, R.color.gray))
+            )
+        }
     }
 
     private fun setClickListeners(
-            courseListener: (Course) -> Unit,
-            addToFavoriteListener: (Course) -> Unit,
-            course: Course
-    ){
+        courseListener: (Course) -> Unit,
+        addToFavoriteListener: (Course) -> Unit,
+        course: Course
+    ) {
         itemView.setOnClickListener { courseListener(course) }
-        binding.imageButton.setOnClickListener {
+        binding.courseFavouriteStar.setOnClickListener {
             addToFavoriteListener(course) // Изменить курс
-
-            binding.imageButton.setImageResource(
-                //TODO отправить все кроме id
-                if (course.isSubscribed) R.drawable.ic_round_star_24
-                else R.drawable.ic_round_star_border_24) }
+        }
     }
 
-    private fun setText(course: Course){
+    private fun setText(course: Course) {
         binding.courseItemFragmentCoursename.text = course.title
         binding.courseItemFragmentLecturescount.text = course.shortDescription
-        binding.imageButton.setImageResource(
-            if (course.isSubscribed) R.drawable.ic_round_star_24
-            else R.drawable.ic_round_star_border_24)
     }
 }
