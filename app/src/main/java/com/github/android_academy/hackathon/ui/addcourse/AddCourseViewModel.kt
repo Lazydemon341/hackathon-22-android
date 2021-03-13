4980f9d224b1d8518895f5fa14ab69c910f95159
Alexander <vlas.s.341@gmail.com>
2021-03-13 13:01:16 +0300

    Open course list on login success

--- app/src/main/java/com/github/android_academy/hackathon/ui/addcourse/AddCourseViewModel.kt
+++ app/src/main/java/com/github/android_academy/hackathon/ui/addcourse/AddCourseViewModel.kt
@@ -1,7 +1,10 @@
 package com.github.android_academy.hackathon.ui.addcourse
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class AddCourseViewModel : ViewModel() {
+class AddCourseViewModel @Inject constructor(
+
+): ViewModel() {
     // TODO: Implement the ViewModel
 }
