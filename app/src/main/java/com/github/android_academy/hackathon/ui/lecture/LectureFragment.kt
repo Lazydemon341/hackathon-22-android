2c304a8690b4e9115bd14c4147deff5d5dcda0db
Freeezzzi <60979041+Freeezzzi@users.noreply.github.com>
2021-03-13 21:43:06 +0300

    add lecture fragment files

---
+++ app/src/main/java/com/github/android_academy/hackathon/ui/lecture/LectureFragment.kt
@@ -0,0 +1,7 @@
+package com.github.android_academy.hackathon.ui.lecture
+
+import com.github.android_academy.hackathon.R
+import com.github.android_academy.hackathon.ui.BaseFragment
+
+class LectureFragment :BaseFragment(R.layout.lection_fragment) {
+}
---
+++ app/src/main/java/com/github/android_academy/hackathon/ui/lecture/LectureViewModel.kt
@@ -0,0 +1,4 @@
+package com.github.android_academy.hackathon.ui.lecture
+
+class LectureViewModel {
+}
