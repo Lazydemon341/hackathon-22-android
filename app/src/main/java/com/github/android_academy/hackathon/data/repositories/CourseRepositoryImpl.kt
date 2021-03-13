810da52504dc810d6c99b328b697522b17a35642
Alexander <vlas.s.341@gmail.com>
2021-03-13 14:49:51 +0300

    Implement CourseRepositor

--- app/src/main/java/com/github/android_academy/hackathon/data/repositories/CourseRepositoryImpl.kt
+++ app/src/main/java/com/github/android_academy/hackathon/data/repositories/CourseRepositoryImpl.kt
@@ -1,8 +1,64 @@
 package com.github.android_academy.hackathon.data.repositories
 
+import com.github.android_academy.hackathon.data.network.ServerApi
+import com.github.android_academy.hackathon.data.network.models.UpdateCourseRequestDTO
+import com.github.android_academy.hackathon.domain.OperationResult
+import com.github.android_academy.hackathon.domain.models.Course
 import com.github.android_academy.hackathon.domain.repositories.CourseRepository
 import javax.inject.Inject
 
 class CourseRepositoryImpl @Inject constructor(
-) : CourseRepository{
+    private val serverApi: ServerApi
+) : CourseRepository {
+
+    override suspend fun getAllCourses(): OperationResult<List<Course>, String?> =
+        try {
+            val courses = serverApi.getAllCourses().map {
+                Course(
+                    id = it.id,
+                    title = it.title,
+                    shortDescription = it.shortDescription,
+                    fullDescription = it.fullDescription,
+                    imgUrl = it.imgUrl,
+                    isSubscribed = it.isSubscribed,
+                    tags = it.tags
+                )
+            }
+            OperationResult.Success(courses)
+        } catch (e: Throwable) {
+            OperationResult.Error(e.message)
+        }
+
+    override suspend fun getFavouriteCourses(username: String): OperationResult<List<Course>, String?> =
+        try {
+            val courses = serverApi.getFavouriteCourses(username).map {
+                Course(
+                    id = it.id,
+                    title = it.title,
+                    shortDescription = it.shortDescription,
+                    fullDescription = it.fullDescription,
+                    imgUrl = it.imgUrl,
+                    isSubscribed = it.isSubscribed,
+                    tags = it.tags
+                )
+            }
+            OperationResult.Success(courses)
+        } catch (e: Throwable) {
+            OperationResult.Error(e.message)
+        }
+
+    override suspend fun updateCourse(course: Course) {
+        serverApi.updateCourse(
+            UpdateCourseRequestDTO(
+                id = course.id,
+                title = course.title,
+                shortDescription = course.shortDescription,
+                fullDescription = course.fullDescription,
+                imgUrl = course.imgUrl,
+                isSubscribed = course.isSubscribed,
+                tags = course.tags
+            )
+        )
+    }
+
 }
--- app/src/main/java/com/github/android_academy/hackathon/domain/repositories/CourseRepository.kt
+++ app/src/main/java/com/github/android_academy/hackathon/domain/repositories/CourseRepository.kt
@@ -1,4 +1,13 @@
 package com.github.android_academy.hackathon.domain.repositories
 
+import com.github.android_academy.hackathon.domain.OperationResult
+import com.github.android_academy.hackathon.domain.models.Course
+
 interface CourseRepository {
+
+    suspend fun getAllCourses(): OperationResult<List<Course>, String?>
+
+    suspend fun getFavouriteCourses(username: String): OperationResult<List<Course>, String?>
+
+    suspend fun updateCourse(course: Course)
 }
