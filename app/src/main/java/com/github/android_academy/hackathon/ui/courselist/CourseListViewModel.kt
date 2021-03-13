9a2b364126f58238c7e62e0548e0001f47bf9c5d
Alexander <vlas.s.341@gmail.com>
2021-03-13 14:00:34 +0300

    refactor

--- app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseListViewModel.kt
+++ app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CourseListViewModel.kt
@@ -3,35 +3,34 @@ package com.github.android_academy.hackathon.ui.courselist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.github.android_academy.hackathon.Screens
 import com.github.android_academy.hackathon.domain.models.Course
 import com.github.android_academy.hackathon.domain.models.User
 import com.github.android_academy.hackathon.domain.repositories.AuthRepository
 import com.github.android_academy.hackathon.domain.repositories.CourseRepository
 import com.github.terrakok.cicerone.Router
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CourseListViewModel @Inject constructor(
-    private val router : Router,
-    private val courseRepository : CourseRepository,
+    private val router: Router,
+    private val courseRepository: CourseRepository,
     private val authRepository: AuthRepository
-) : ViewModel(){
+) : ViewModel() {
     //TODO: implement view model
     private val _mutableuser = MutableLiveData<User>(authRepository.loadUser())
 
     val user: LiveData<User> get() = _mutableuser
 
-    fun onCourseAction(course: Course){
+    fun onCourseAction(course: Course) {
         //TODO получить курс
     }
 
-    fun addCourseAction(){
-        //TODO добавление курса. На то что ментор уже проверили
+    fun addCourseAction() {
+        router.navigateTo(Screens.AddCourseFragment())
     }
 
-    fun addToFavoriteAction(course: Course){
-        //TODO обновить юзера, обновить иконку
+    fun addToFavoriteAction(course: Course) {
+        //TODO обновить юзера
     }
 
     fun isMentor() = user.value?.isMentor == true
--- app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CoursesAdapter.kt
+++ app/src/main/java/com/github/android_academy/hackathon/ui/courselist/CoursesAdapter.kt
@@ -8,9 +8,9 @@ import com.github.android_academy.hackathon.databinding.CourseItemFragmentBindin
 import com.github.android_academy.hackathon.domain.models.Course
 
 class CoursesAdapter(
-        private val courseListener: (Course) -> Unit, //Обозначает листенера для нажатия на курс
-        private val addToFavoriteListener: (Course) -> Unit
-) : ListAdapter<Course, CourseViewHolder>(DIFF_CALLBACK){
+    private val courseListener: (Course) -> Unit, //Обозначает листенера для нажатия на курс
+    private val addToFavoriteListener: (Course) -> Unit
+) : ListAdapter<Course, CourseViewHolder>(DIFF_CALLBACK) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = CourseItemFragmentBinding.inflate(layoutInflater, parent, false)
@@ -18,12 +18,17 @@ class CoursesAdapter(
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-        holder.bind(getItem(position), courseListener = courseListener, addToFavoriteListener = addToFavoriteListener)
+        holder.bind(
+            getItem(position),
+            courseListener = courseListener,
+            addToFavoriteListener = addToFavoriteListener
+        )
     }
-    companion object{
-        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Course>(){
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Course>() {
             override fun areItemsTheSame(oldItem: Course, newItem: Course): Boolean =
-                oldItem.imgUrl == newItem.imgUrl
+                oldItem.title == newItem.title
 
             override fun areContentsTheSame(oldItem: Course, newItem: Course): Boolean =
                 oldItem == newItem
